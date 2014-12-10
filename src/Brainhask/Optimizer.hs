0dd319b09b02bd32dba6e96f5dbeb116ae200039
damianfral <huevofritopamojarpan@gmail.com>
2014-12-10 22:13:03 +0100

    Use Applicatives instead of Monads (when readability isn't reduced).

--- src/Brainhask/Optimizer.hs
+++ src/Brainhask/Optimizer.hs
@@ -9,34 +9,28 @@ import Control.Applicative hiding (many)
 import Control.Monad
 import Data.Data
 import Data.Functor
+import Data.Monoid
 import Data.List
 import Data.Transformer
 import Data.Typeable
 
+ffmap  = fmap . fmap
+sumOps = getSum . extract . ffmap Sum
+
 type BFTransformer = Transformer (Op Int) (Op Int)
 
 reduceModifies :: BFTransformer
-reduceModifies = do
-    ms <- many1 $ matchConstructor (Modify 1)
-    return $ Modify $ sum $ map (\(Modify x) -> x) ms
+reduceModifies = Modify . sumOps <$> (many1 $ matchConstructor (Modify 0))
 
 reduceLoops :: BFTransformer
-reduceLoops = satisfies (== (Loop [Modify (-1)]))  *> pure (Set 0)
+reduceLoops = satisfies (== (Loop [Modify (-1)])) *> pure (Set 0)
 
 reduceMoves :: BFTransformer
-reduceMoves = do
-    ms <- many1 $ matchConstructor (Move 1)
-    return $ Move $ sum $ map (\(Move x) -> x) ms
+reduceMoves = Move . sumOps <$> (many1 $ matchConstructor (Move 0))
 
 removeModifies0, removeMoves0  :: BFTransformer
-
-removeModifies0 = do
-    many1 $ match (Modify 0)
-    return NoOp
-
-removeMoves0    = do
-    many1 $ match (Move 0)
-    return NoOp
+removeModifies0 = (many1 $ match (Modify 0)) *> pure NoOp
+removeMoves0    = (many1 $ match (Move 0))   *> pure NoOp
 
 optimizeLoops :: [BFTransformer] -> BFTransformer
 optimizeLoops opts = do
@@ -44,5 +38,4 @@ optimizeLoops opts = do
     return $ Loop $ mtransform opts ops
 
 allOpt = [reduceModifies, reduceMoves, optimizeLoops allOpt, removeModifies0, removeMoves0, reduceLoops]
-
 optimize = filter (/= NoOp) . mtransform allOpt
